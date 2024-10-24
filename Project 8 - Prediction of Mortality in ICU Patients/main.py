9dbc74bb8c38dac04b453b90511a1a394aa7b401
andreuolaortua <andreu.olaortua23@gmail.com>
2024-10-24 17:22:03 +0200

    Second commit

--- Project 8 - Prediction of Mortality in ICU Patients/main.py
+++ Project 8 - Prediction of Mortality in ICU Patients/main.py
@@ -4,15 +4,3 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-#Importing dataset
-data = pd.read_csv('XXX.csv')
-x = dataset.iloc[].values
-y = dataset.iloc[].values
-
-#Splitting dataset into training set and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-
-#Feature Scaling
-sc = StandardScaler()
-X_train = 
-X_test = 
